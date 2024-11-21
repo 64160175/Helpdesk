99deda3d6354dfeafd140e8ea6ba6c0d8174d749
64160175 <64160175@go.buu.ac.th>
2024-11-21 16:34:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// ตั้งค่า view engine เป็น EJS
+app.set('view engine', 'ejs');
+
+// ตั้งค่า directory สำหรับ views
+app.set('views', path.join(__dirname, 'views'));
+
+// ตั้งค่า static directory สำหรับไฟล์ CSS และอื่นๆ
+app.use(express.static(path.join(__dirname, 'public')));
+
+// เส้นทางหลักที่จะแสดงหน้า login
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
