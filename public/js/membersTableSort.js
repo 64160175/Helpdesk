d6b3732f08e6c195c0e1d3383a5a2cd877c022c2
64160175 <64160175@go.buu.ac.th>
2024-11-25 14:19:17 +0700

    memberpage 3

---
+++ public/js/membersTableSort.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const membersTable = document.getElementById('membersTable');
+    if (membersTable) {
+        new simpleDatatables.DataTable(membersTable);
+    }
+});
+
+function sortTable(columnIndex) {
+    const table = document.querySelector("table tbody");
+    const rows = Array.from(table.rows);
+
+    rows.sort((a, b) => {
+        const cellA = a.cells[columnIndex].innerText;
+        const cellB = b.cells[columnIndex].innerText;
+
+        if (!isNaN(Date.parse(cellA)) && !isNaN(Date.parse(cellB))) {
+            return new Date(cellA) - new Date(cellB);
+        }
+
+        return cellA.localeCompare(cellB, 'th', { numeric: true });
+    });
+
+    rows.forEach(row => table.appendChild(row));
+}
